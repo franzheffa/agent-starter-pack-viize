971c853f327ba08cd9ef66ef8fdcf7370c040e4a
franz heffa <franzheffa@buttertech.io>
2025-12-22 01:39:49 -0500

    fix(build): simplify prisma imports to local api directory

--- blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/post/[id].ts
+++ blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/post/[id].ts
@@ -1,15 +1,11 @@
-import prisma from '../../../lib/prisma';
+import prisma from '../prisma';
 
 export default async function handle(req, res) {
   const postId = String(req.query.id);
   if (req.method === 'DELETE') {
-    const post = await prisma.post.delete({
-      where: { id: postId },
-    });
+    const post = await prisma.post.delete({ where: { id: postId } });
     res.json(post);
   } else {
-    throw new Error(
-      "The method " + req.method + " is not allowed at this route."
-    );
+    throw new Error("Method " + req.method + " not allowed.");
   }
 }
