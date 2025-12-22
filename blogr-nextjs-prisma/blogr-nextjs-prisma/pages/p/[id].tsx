b12f2cc61d5654f03c09a266b7f69a5a49c9c278
franz heffa <franzheffa@buttertech.io>
2025-12-22 01:55:17 -0500

    fix(frontend): align all page imports with stabilized prisma instance

--- blogr-nextjs-prisma/blogr-nextjs-prisma/pages/p/[id].tsx
+++ blogr-nextjs-prisma/blogr-nextjs-prisma/pages/p/[id].tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { GetServerSideProps } from 'next';
 import ReactMarkdown from 'react-markdown';
-import Router from 'next/router';
 import Layout from '../../components/Layout';
 import { PostProps } from '../../components/Post';
-import { useSession } from 'next-auth/react';
-import prisma from '../../lib/prisma';
+import prisma from '../api/prisma';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
-    where: { id: String(params?.id) },
-    include: { author: { select: { name: true, email: true } } },
+    where: {
+      id: String(params?.id),
+    },
+    include: {
+      author: true,
+    },
   });
-  return { props: post };
+  return {
+    props: post ?? {},
+  };
 };
 
-async function publishPost(id: string) { await fetch(`/api/publish/${id}`, { method: 'PUT' }); await Router.push('/'); }
-async function deletePost(id: string) { await fetch(`/api/post/${id}`, { method: 'DELETE' }); await Router.push('/'); }
-
 const Post: React.FC<PostProps> = (props) => {
-  const { data: session, status } = useSession();
-  if (status === 'loading') return <div>Authenticating ...</div>;
-  const userHasValidSession = Boolean(session);
-  const postBelongsToUser = session?.user?.email === props.author?.email;
-  const title = props.published ? props.title : `${props.title} (Draft)`;
+  let title = props.title;
+  if (!props.published) {
+    title = `${title} (Draft)`;
+  }
 
   return (
     <Layout>
@@ -31,16 +31,9 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown children={props.content || ''} />
-        {!props.published && userHasValidSession && postBelongsToUser && (<button onClick={() => publishPost(props.id)}>Publish</button>)}
-        {userHasValidSession && postBelongsToUser && (<button onClick={() => deletePost(props.id)}>Delete</button>)}
       </div>
-      <style jsx>{`
-        .page { background: var(--geist-background); padding: 2rem; }
-        .actions { margin-top: 2rem; }
-        button { background: #ececec; border: 0; border-radius: .125rem; padding: 1rem 2rem; }
-        button + button { margin-left: 1rem; }
-      `}</style>
     </Layout>
   );
 };
+
 export default Post;
