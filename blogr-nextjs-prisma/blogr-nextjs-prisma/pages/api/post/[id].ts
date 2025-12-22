15b2bed4ed6b8648f4f9db67cc39c46e13724cc8
franz heffa <franzheffa@buttertech.io>
2025-12-22 01:20:12 -0500

    fix(api): align all prisma import paths to relative levels

--- blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/post/[id].ts
+++ blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/post/[id].ts
@@ -1,10 +1,15 @@
-import prisma from '../../../lib/prisma';
+import prisma from '../../lib/prisma';
 
 export default async function handle(req, res) {
   const postId = String(req.query.id);
   if (req.method === 'DELETE') {
-    const post = await prisma.post.delete({ where: { id: postId } });
-    return res.json(post);
+    const post = await prisma.post.delete({
+      where: { id: postId },
+    });
+    res.json(post);
+  } else {
+    throw new Error(
+      "The method " + req.method + " is not allowed at this route."
+    );
   }
-  res.status(405).end();
 }
--- blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/publish/[id].ts
+++ blogr-nextjs-prisma/blogr-nextjs-prisma/pages/api/publish/[id].ts
@@ -1,8 +1,10 @@
-import prisma from '../../../lib/prisma';
+import prisma from '../../lib/prisma';
 
 export default async function handle(req, res) {
   const postId = String(req.query.id);
-  if (req.method !== 'PUT') return res.status(405).end();
-  const post = await prisma.post.update({ where: { id: postId }, data: { published: true } });
+  const post = await prisma.post.update({
+    where: { id: postId },
+    data: { published: true },
+  });
   res.json(post);
 }
