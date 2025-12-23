baecabd2f2e57bbf68918c35005b9f44dad5965f
franz heffa <franzheffa@buttertech.io>
2025-12-23 03:14:56 -0500

    fix(infra): align root directory and finalize database connection

--- blogr-nextjs-prisma/blogr-nextjs-prisma/blogr-nextjs-prisma/next.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Ajuste si tu utilises des images distantes :
-  images: { remotePatterns: [] },
-};
-module.exports = nextConfig;
