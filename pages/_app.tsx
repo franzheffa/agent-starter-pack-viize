a55d2bb0b3548f20a91c1ef9298e8a5e1b68281f
franz heffa <franzheffa@buttertech.io>
2025-12-24 19:07:39 -0500

    fix(auth): wrap application in SessionProvider to fix prerendering error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import Head from 'next/head';
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../globals.css';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Head>
         <title>Buttertech | Agent Smith-Heffa Multimodal AI</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Montreal-based Deep Tech startup for autonomous surveillance." />
       </Head>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
